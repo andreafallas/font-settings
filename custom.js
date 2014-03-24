bda7538dfc5e97b284691c23218e44181364bddb
andreafallas <andrea.fallas@gmail.com>
2014-03-24 22:58:36 +0000

    Responsive tweaks and general tidying up
    Added some grouping and padding to settings buttons, moved JS into
    separate file

---
+++ custom.js
@@ -0,0 +1,11 @@
+function change_text() {
+	var button = document.getElementById('hide_show_btn');
+
+	if (button.innerHTML === "Hide instructions") {
+		button.innerHTML = "Show instructions";
+	}
+	else {
+		button.innerHTML = "Hide instructions";
+	}
+}
+
