15b37b0a424d8d937cbd6f9ef2956c3e417d57bc
andreafallas <andrea.fallas@gmail.com>
2014-03-25 01:34:01 +0000

    Moved text resizing functionality

--- custom.js
+++ custom.js
@@ -8,3 +8,18 @@ function change_text() {
 		button.innerHTML = "Hide instructions";
 	}
 }
+
+function changeFontSize(id,step) {
+	step = parseInt(step,10);
+	var x = document.getElementById(id);
+	var currSize = parseInt(x.style.fontSize,10);
+	x.style.fontSize = (currSize+step) + 'px';
+}
+
+function serif() {
+	document.getElementById('text').style.fontFamily="Georgia, Times, Times New Roman, serif";
+}
+
+function sans() {
+	document.getElementById('text').style.fontFamily="Lato,Helvetica,Arial,sans-serif";
+}
